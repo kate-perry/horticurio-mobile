ff25d43ca0c6fdb0a591981f8fc8ecce15a75c0c
Kate <kate.perry@zoho.com>
2020-07-09 00:46:32 -0400

    plant models, color and some icons

---
+++ horticurio/horticurio/Models/MyPlant.cs
@@ -0,0 +1,10 @@
+﻿namespace horticurio.Models
+{
+    class MyPlant
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
